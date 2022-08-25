f10db9ca22269076af9d062ece5aa385155818c7
Cristian <sastoque88@gmail.com>
2022-08-25 01:21:02 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MoviesComponent } from './movies/movies.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: WelcomeComponent},
+  {path: 'movies', component: MoviesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { SearcherService } from '../services/searcher.service';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.component.html',
+  styleUrls: ['./movies.component.css']
+})
+export class MoviesComponent implements OnInit {
+
+  constructor(private searcher: SearcherService) { }
+  
+  movies: any[] = [];
+
+  ngOnInit(): void {
+  }
+
+  async onMovieSelected(movie: string){
+    try{
+      const req: any = await this.searcher.getMovies(movie);
+      if(req.total_results>0) this.movies = req.results;
+      console.log(req.results);
+    }catch(e){
+      console.log(e);
+    }
+
+  }
+
+}
